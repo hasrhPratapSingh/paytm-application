fd45c7c54177854e242bddcad3f50d406801859c
Harsh Pratap Singh <harshpratapsingh.bt21aero@pec.edu.in>
2024-10-29 16:58:39 +0530

    some changes

--- Backend/routes/account.js
+++ Backend/routes/account.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const authMiddleware = require('../middleware');
 const { Account } = require('../db');
+const { default: mongoose } = require('mongoose');
 
 const router =express.Router();
 
 
 router.get('/balance',authMiddleware,async (req,res)=>{
+  res.json({msg:"hi"})
   const account = await Account.findOne({
     userId: req.userId
   });
