9a8a6fbd1fab967816a2e19f95d612d91c585c3d
Harsh Pratap Singh <harshpratapsingh.bt21aero@pec.edu.in>
2024-10-29 14:04:42 +0530

    changes in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mainRouter = require("./routes/index")
 const cors = require('cors');
-const app = express;
+const app = express();
 
 app.use(cors());
 app.use(express.json());
