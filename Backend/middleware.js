8ba62e310872b7824fa357a1058e901b5326a0fc
Harsh Pratap Singh <harshpratapsingh.bt21aero@pec.edu.in>
2024-11-03 01:23:41 +0530

    chnages in middleware

--- Backend/middleware.js
+++ Backend/middleware.js
@@ -3,9 +3,9 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("./config");
 
 const authMiddleware = (req, res,next)=>{
-  const authHeader = req.header.authorization;
+  const authHeader = req.headers.authorization;
 
-  if(!authHeader || !authHeader.startsWith("Bearer")){
+  if(!authHeader || !authHeader.startsWith("Bearer ")){
     return res.status(403).json({});
   }
 
